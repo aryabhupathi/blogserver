0a567224fd903928173a4ab8a0e9ff4de5bc6d67
Arya Bhupathi <bhupathikonduru@gmail.com>
2024-11-10 15:30:12 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const commentRouter = require("./routes/commentRoute");
 const replyRouter = require("./routes/replyRoute");
 const userRouter = require("./routes/userRoute");
 const loginRouter = require("./routes/loginRoute");
-const path = require("path");
+// const path = require("path");
 // const dotenv = require('dotenv');
 // dotenv.config();
 const app = express();
@@ -22,11 +22,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!"); // Or render a page
 });
 
-app.use("/api/post", postsRouter);
-app.use("/api/comment", commentRouter);
-app.use("/api/reply", replyRouter);
-app.use("/api/user", userRouter);
-app.use("/api/login", loginRouter);
+app.get("/api/post", postsRouter);
+app.get("/api/comment", commentRouter);
+app.get("/api/reply", replyRouter);
+app.get("/api/user", userRouter);
+app.get("/api/login", loginRouter);
 
   mongoose.connect('mongodb+srv://vercel-admin-user:Mepassword123@cluster0.xvmqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 // Start the server

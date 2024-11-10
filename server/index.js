f45f7c3f1d7db824b4f23df046dcce737da70cdf
Arya Bhupathi <bhupathikonduru@gmail.com>
2024-11-10 14:40:44 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const replyRouter = require("./routes/replyRoute");
 const userRouter = require("./routes/userRoute");
 const loginRouter = require("./routes/loginRoute");
 const path = require("path");
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 const app = express();
 
 // Middleware
@@ -35,7 +35,7 @@ app.use('/uploads', express.static('uploads'));
 //   .catch((err) => console.log(err));
 
 
-  mongoose.connect('mongodb+srv://vercel-admin-user:Mepassword123@cluster0.xvmqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0JWT_SECRET=trynewpassword');
+  mongoose.connect('mongodb+srv://vercel-admin-user:Mepassword123@cluster0.xvmqj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 // Start the server
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
